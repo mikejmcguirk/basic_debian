9cbc062f9ec622605c1fc9439fd260372921691a
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-03-20 11:40:50 -0400

    Updated to handle ssh and a bunch of other stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-###############################
+#!/bin/bash
+
 # Meant for Debian Linux Severs
-###############################
+# NOTE: This script leaves password authentication enabled for SSH
+# Instructions for setting up key-based auth are at the end
+
+set -e
 
 apt update
 apt upgrade -y
@@ -11,8 +15,25 @@ if ! systemctl is-active systemd-timesyncd >/dev/null; then
     systemctl start systemd-timesyncd
 fi
 
+if [ -n "$SUDO_USER" ]; then
+    USER_HOME="/home/$SUDO_USER"
+    if [ ! -d "$USER_HOME/.ssh" ]; then
+        mkdir -p "$USER_HOME/.ssh"
+        chown "$SUDO_USER":"$SUDO_USER" "$USER_HOME/.ssh"
+        chmod 700 "$USER_HOME/.ssh"
+    fi
+    touch "$USER_HOME/.ssh/authorized_keys"
+    chown "$SUDO_USER":"$SUDO_USER" "$USER_HOME/.ssh/authorized_keys"
+    chmod 600 "$USER_HOME/.ssh/authorized_keys"
+
+    echo 'PATH="$PATH:/sbin"' >> "/home/$SUDO_USER/.bashrc"
+    echo "alias ls='ls -lhA --color=auto'" >> "/home/$SUDO_USER/.bashrc"
+    chown "$SUDO_USER":"$SUDO_USER" "/home/$SUDO_USER/.bashrc"
+else
+    echo "Warning: SUDO_USER not set. Skipping .bashrc modifications."
+fi
+
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 sed -i 's/#LogLevel INFO/LogLevel VERBOSE/' /etc/ssh/sshd_config
 echo "MaxAuthTries 3" >> /etc/ssh/sshd_config
 systemctl restart ssh
@@ -29,15 +50,13 @@ systemctl enable fail2ban
 systemctl restart fail2ban
 
 # TODO: Will need to add rules for http. 80/tcp for http and 443/tcp for https
-# Do we need to allow http or no?
-# Grok reasonably suggests having web processes confined to a specifically privileged user
-# Suggests AppArmor/SELinux to "prevent escalation"
-# "Add profiles" for Nginx/Apache
+# Do we even need to allow http or no?
 apt install -y ufw
 ufw default deny incoming # Should be default, but let's be sure
 ufw default allow outgoing # Also should be default
 ufw allow 22/tcp
 ufw limit 22/tcp
+ufw allow proto icmp icmptype 8
 ufw logging on
 ufw --force enable
 
@@ -52,13 +71,9 @@ fi
 if ! systemctl is-active auditd >/dev/null; then
     systemctl start auditd
 fi
-# Maybe do these
-# echo 'export PATH="$PATH:/sbin"' >> ~/.bashrc
-# source ~/.bashrc
 /sbin/auditctl -e 1
 /sbin/auditctl -a always,exit -F path=/etc/ssh/sshd_config
 
-echo "blacklist floppy" |  tee -a /etc/modprobe.d/blacklist.conf > /dev/null
 echo "blacklist floppy" |  tee -a /etc/modprobe.d/blacklist.conf > /dev/null
 echo "blacklist firewire-core" |  tee -a /etc/modprobe.d/blacklist.conf > /dev/null
 echo "blacklist bluetooth" |  tee -a /etc/modprobe.d/blacklist.conf > /dev/null
@@ -76,15 +91,26 @@ echo "Upgrade completed at $(date)" >> "$LOGFILE"
 echo "Check if reboot is needed: /var/run/reboot-required" >> "$LOGFILE"
 EOF
 chmod +x /usr/local/bin/upgrade-server.sh
-echo "0 2 * * * /usr/local/bin/upgrade-server.sh" | crontab -
+echo "0 4 * * * /usr/local/bin/upgrade-server.sh" | crontab -
 # if [ -f /var/run/reboot-required ]; then echo "Reboot needed"; else echo "No reboot needed"; fi
 # cat /var/run/reboot-required
 # tail /var/log/server-upgrades.log
 
+apt install -y apparmor apparmor-profiles
+systemctl enable apparmor
+systemctl start apparmor
 
 echo "Server setup complete"
 
-# TODO: How do I pull the script in?
+# After running:
+# On your machine: ssh-keygen -t ed25519 -f your_ssh_key
+# On your machine: scp your_ssh_key.pub user@server.address:/home/user/.ssh
+# On the server: cat your_ssh_key.pub >> authorized_keys
+# On your machine: Test ssh with key
+# On the VM: sudo vim /etc/ssh/sshd_config
+# Change #PasswordAuthentication yes to PasswordAuthentication no
+# sudo systemctl restart ssh
+# On your machine: Rename the keys to ed25519 and ed25519.pub
 
 # TODO: Maybe do 2FA for SSH, but seems a bit extra
 # TODO: Possibly use aide or Tripwire to detect file changes. Or Prometheus/SIEM
